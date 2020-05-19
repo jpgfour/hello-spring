09cfaf7d3a68b8d06150662421d560b036f94e2b
jpgfour <jeremy.grise@gmail.com>
2020-05-18 20:14:16 -0500

    hello-spring exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -58,30 +58,30 @@ public class HelloController {
                 "<form action='/hello' method='get'>" + //submit a request to /hello
                 "<input type='text' name='name'>" +
                 //implement dropdown AKA select
-//                "<select name=\"hellos\" id=\"hellos-select\">\n" +
-//                "    <option value=\"\">--Please choose an option--</option>\n" +
-//                "    <option value=\"french\">French</option>\n" +
-//                "    <option value=\"spanish\">Spanish</option>\n" +
-//                "    <option value=\"german\">German</option>\n" +
-//                "    <option value=\"italian\">Italian</option>\n" +
-//                "    <option value=\"portuguese\">Portuguese</option>\n" +
-//                "    <option value=\"hindi\">Hindi</option>\n" +
-//                "    <option value=\"persian-farsi\">Persian (Farsi)</option>\n" +
-//                "    <option value=\"russian\">Russian</option>\n" +
-//                "    <option value=\"japanese\">Japanese</option>\n" +
-//                "    <option value=\"korean\">Korean</option>\n" +
-//                "    <option value=\"turkish\">Turkish</option>\n" +
-//                "    <option value=\"mongolian\">Mongolian</option>\n" +
-//                "    <option value=\"kazakh\">Kazakh</option>\n" +
-//                "    <option value=\"hungarian\">Hungarian</option>\n" +
-//                "    <option value=\"arabic\">Arabic</option>\n" +
-//                "    <option value=\"hausa\">Hausa</option>\n" +
-//                "    <option value=\"swahili\">Swahili</option>\n" +
-//                "    <option value=\"mandarin\">Mandarin</option>\n" +
-//                "    <option value=\"cantonese\">Cantonese</option>\n" +
-//                "    <option value=\"bahasa-indonesia\">Bahasa Indonesia</option>\n" +
-//                "    <option value=\"english\">English</option>\n" +
-//                "</select>" +
+                "<select name=\"hellos\" id=\"hellos-select\">\n" +
+                "    <option value=\"\">--Please choose an option--</option>\n" +
+                "    <option value=\"french\">French</option>\n" +
+                "    <option value=\"spanish\">Spanish</option>\n" +
+                "    <option value=\"german\">German</option>\n" +
+                "    <option value=\"italian\">Italian</option>\n" +
+                "    <option value=\"portuguese\">Portuguese</option>\n" +
+                "    <option value=\"hindi\">Hindi</option>\n" +
+                "    <option value=\"persian-farsi\">Persian (Farsi)</option>\n" +
+                "    <option value=\"russian\">Russian</option>\n" +
+                "    <option value=\"japanese\">Japanese</option>\n" +
+                "    <option value=\"korean\">Korean</option>\n" +
+                "    <option value=\"turkish\">Turkish</option>\n" +
+                "    <option value=\"mongolian\">Mongolian</option>\n" +
+                "    <option value=\"kazakh\">Kazakh</option>\n" +
+                "    <option value=\"hungarian\">Hungarian</option>\n" +
+                "    <option value=\"arabic\">Arabic</option>\n" +
+                "    <option value=\"hausa\">Hausa</option>\n" +
+                "    <option value=\"swahili\">Swahili</option>\n" +
+                "    <option value=\"mandarin\">Mandarin</option>\n" +
+                "    <option value=\"cantonese\">Cantonese</option>\n" +
+                "    <option value=\"bahasa-indonesia\">Bahasa Indonesia</option>\n" +
+                "    <option value=\"english\">English</option>\n" +
+                "</select>" +
                 "<input type ='submit' value='Greet me!'>" +
                 "</form>" +
                 "</body>" +
