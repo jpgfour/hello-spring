24b3cfcb618d33f0c39d602ca1f258f156e3563c
jpgfour <jeremy.grise@gmail.com>
2020-06-01 09:23:19 -0500

    Working on class 10 - adding templates

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
+//@ResponseBody
 @RequestMapping("hello")
 public class HelloController {
 
@@ -26,6 +26,7 @@ public class HelloController {
 //    @GetMapping("hello")
 //    @PostMapping("hello")
     //@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+
     public String helloWithQueryParam(@RequestParam String name) {
 
         return "Hello, " + "this is not the form response, " + name + "!";
@@ -38,6 +39,7 @@ public class HelloController {
     }
 
     //TODO Handles requests of the the form /hello?name=LaunchCode&hellos=English
+    @ResponseBody
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public static String createMessage(@RequestParam String name, @RequestParam String hellos){
 
@@ -67,56 +69,48 @@ public class HelloController {
     }
 
 
+//     /hello/form
+    @GetMapping("form")
+    public String helloForm(){
+        return "form";
+    }
+
     // /hello/form
 //    @GetMapping("form")
-//    public String helloForm(){
+//    public String helloAroundTheWorldForm(){
 //        return "<html" +
 //                "<body>" +
-//                //OMG 'hello' in the next line needed to '/hello'
 //                "<form action='/hello' method='get'>" + //submit a request to /hello
 //                "<input type='text' name='name'>" +
+//                //implement dropdown AKA select
+//                "<select name=\"hellos\" id=\"hellos-select\">\n" +
+//                "    <option value=\"\">--Please choose an option--</option>\n" +
+//                "    <option value=\"french\">French</option>\n" +
+//                "    <option value=\"spanish\">Spanish</option>\n" +
+//                "    <option value=\"german\">German</option>\n" +
+//                "    <option value=\"italian\">Italian</option>\n" +
+//                "    <option value=\"portuguese\">Portuguese</option>\n" +
+//                "    <option value=\"hindi\">Hindi</option>\n" +
+//                "    <option value=\"persian-farsi\">Persian (Farsi)</option>\n" +
+//                "    <option value=\"russian\">Russian</option>\n" +
+//                "    <option value=\"japanese\">Japanese</option>\n" +
+//                "    <option value=\"korean\">Korean</option>\n" +
+//                "    <option value=\"turkish\">Turkish</option>\n" +
+//                "    <option value=\"mongolian\">Mongolian</option>\n" +
+//                "    <option value=\"kazakh\">Kazakh</option>\n" +
+//                "    <option value=\"hungarian\">Hungarian</option>\n" +
+//                "    <option value=\"arabic\">Arabic</option>\n" +
+//                "    <option value=\"hausa\">Hausa</option>\n" +
+//                "    <option value=\"swahili\">Swahili</option>\n" +
+//                "    <option value=\"mandarin\">Mandarin</option>\n" +
+//                "    <option value=\"cantonese\">Cantonese</option>\n" +
+//                "    <option value=\"bahasa-indonesia\">Bahasa Indonesia</option>\n" +
+//                "    <option value=\"english\">English</option>\n" +
+//                "</select>" +
 //                "<input type ='submit' value='Greet me!'>" +
 //                "</form>" +
 //                "</body>" +
 //                "</html>";
 //    }
-//
-    // /hello/form
-    @GetMapping("form")
-    public String helloAroundTheWorldForm(){
-        return "<html" +
-                "<body>" +
-                "<form action='/hello' method='get'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                //implement dropdown AKA select
-                "<select name=\"hellos\" id=\"hellos-select\">\n" +
-                "    <option value=\"\">--Please choose an option--</option>\n" +
-                "    <option value=\"french\">French</option>\n" +
-                "    <option value=\"spanish\">Spanish</option>\n" +
-                "    <option value=\"german\">German</option>\n" +
-                "    <option value=\"italian\">Italian</option>\n" +
-                "    <option value=\"portuguese\">Portuguese</option>\n" +
-                "    <option value=\"hindi\">Hindi</option>\n" +
-                "    <option value=\"persian-farsi\">Persian (Farsi)</option>\n" +
-                "    <option value=\"russian\">Russian</option>\n" +
-                "    <option value=\"japanese\">Japanese</option>\n" +
-                "    <option value=\"korean\">Korean</option>\n" +
-                "    <option value=\"turkish\">Turkish</option>\n" +
-                "    <option value=\"mongolian\">Mongolian</option>\n" +
-                "    <option value=\"kazakh\">Kazakh</option>\n" +
-                "    <option value=\"hungarian\">Hungarian</option>\n" +
-                "    <option value=\"arabic\">Arabic</option>\n" +
-                "    <option value=\"hausa\">Hausa</option>\n" +
-                "    <option value=\"swahili\">Swahili</option>\n" +
-                "    <option value=\"mandarin\">Mandarin</option>\n" +
-                "    <option value=\"cantonese\">Cantonese</option>\n" +
-                "    <option value=\"bahasa-indonesia\">Bahasa Indonesia</option>\n" +
-                "    <option value=\"english\">English</option>\n" +
-                "</select>" +
-                "<input type ='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
-    }
 
 }
